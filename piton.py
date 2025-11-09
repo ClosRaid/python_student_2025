a9b3043c81b6073fc8157a5aec5196f820871403
ClosRaid <ad-ad2000@inbox.ru>
2025-11-09 19:15:41 +0500

    09.11

--- piton.py
+++ piton.py
@@ -583,114 +583,97 @@ import random
 
 
 #30.10 задание до 2.11
-def formativ():
-    print("Don't compare your \n"
-          "with anyone in this world... \n"
-          "if you do so, you are insulting your self \n"
-          "\n",
-          "\t"* 3, "Bill Gates" )
-def value():
-    value1 = int(input("введите число"))
-    value2 = int(input("введите второе число"))
-    for i in range(value1,value2):
-        if i % 2 == 0:
-            print(f"{i},Чётные числа в радиусе от {value1} до {value2}")
-        else:
-            print(f"в радиусе от {value1} до {value2} чётных чисел не обнаружено")
-def square():
-        square_value = int(input("введите длину стороны квадрата "))
-        symbol = input("введите символ ")
-        znak = int(input("введите переменную логического типа 1: истина 2: ложь "))
-        if znak >= 3:
-            print("incorrect value")
-        for i in range(square_value):
-            print(symbol * i,symbol * i,sep = '', end = '')
-            print(symbol, "\t", symbol)
-            print(symbol * i,symbol * i,sep = '', end = '')
-square()
-'''
-методы для работы с файлом
-file.close()
-file.fileno()
-file.flush() - метод очистки буфера
-file.isatty() - проверка на открытие файла в терминале()прямо сейчас)
-file.next() - возвращает след. строку файла
-file.read()
-file.readline() - чтение строки файла
-file.readlines() - чтение всех строк файла
-file.seek()
-file.seekable() - роверка доступа файла
-file.tell() - текущая позиция в строке файла
-file.truncate() - уменьшает размер файла на n байт
-file.write()
-file.writelines(line) - обавление строк в файл
-'''
+# def formativ():
+#     print("Don't compare your \n"
+#           "with anyone in this world... \n"
+#           "if you do so, you are insulting your self \n"
+#           "\n",
+#           "\t"* 3, "Bill Gates" )
+# def value():
+#     value1 = int(input("введите число"))
+#     value2 = int(input("введите второе число"))
+#     for i in range(value1,value2):
+#         if i % 2 == 0:
+#             print(f"{i},Чётные числа в радиусе от {value1} до {value2}")
+#         else:
+#             print(f"в радиусе от {value1} до {value2} чётных чисел не обнаружено")
+# def square():
+#         square_value = int(input("введите длину стороны квадрата "))
+#         symbol = input("введите символ ")
+#         znak = int(input("введите переменную логического типа 1: истина 2: ложь "))
+#         if znak >= 3:
+#             print("incorrect value")
+#         for i in range(square_value):
+#             print(symbol * i,symbol * i,sep = '', end = '')
+#             print(symbol, "\t", symbol)
+#             print(symbol * i,symbol * i,sep = '', end = '')
+# square()
 #30.10
-import re
-def isCyrillic(text):
-    return bool(re.search('[а-яА-Я]', text))
-def scrabl():
-    point_en = {1:'AEIOULNSTR',
-                    2:'DG',
-                    3:'BCMP',
-                    4:'FHVWY',
-                    5:'K',
-                    8:'JX',
-                    10:'QZ'}
-    point_ru = {1:'АВЕИНОСТ',
-                2:'ДКЛМПУ',
-                3:'БГЁЬЯ',
-                4:'ЙЫ',
-                5:'ЖЗЧЦЧ',
-                8:'ШЭЮ',
-                10:'ФЩЪ'}
-    while True:
-        text_user = input("Для отмены напишите 0. Игрок 1 Введите слово:").upper()
-        if text_user == "0":
-            print("Прощайте")
-            break
-        text_user2 = input("Для отмены напишите 0. Игрок 2 Введите слово:").upper()
-        if text_user2 == "0":
-            print("Прощайте")
-            break
-        player1 = 0
-        player2 = 0
-        if isCyrillic(text_user):
-                print('Кол-во очков:',sum([key for i in text_user for key, value in point_ru.items() if i in value]))
-                player1 = player1 + sum([key for i in text_user for key, value in point_ru.items() if i in value])
-                player2 = player2 + sum([key for i in text_user2 for key, value in point_ru.items() if i in value])
-        else:
-                print('Кол-во очков:',sum([key for i in text_user for key, value in point_en.items() if i in value]))
-                player1 = player1 + sum([key for i in text_user for key, value in point_en.items() if i in value])
-                player2 = player2 + sum([key for i in text_user2 for key, value in point_en.items() if i in value])
-        print(f"кол-во очков первого игрока {player1}, кол-во очков второго игрока {player2} ")
-def camping():
-    backpack = {'Зажигалка': 20, 'Компас': 100, 'Фрукты': 500, 'Рубашка': 300,
-                'Термос': 1000, 'аптечка': 200, 'Куртка': 600, 'Бинокль': 400,
-                'Удочка': 1300, 'Салфетки': 40, 'Бутерброды': 800, 'Палатка': 5500,
-                'Спальный мешок': 2500, 'Изолента': 250, 'Котел': 3000
-                }
-    human1_massa = int(input("Введите допустимый вес рюкзака первого человека: ")) * 1000
-    human2_massa = int(input("Введите допустимый вес рюкзака первого человека: ")) * 1000
-    print("Могу взять: ")
-    for key, value in backpack.items():
-        if value < human1_massa or value < human2_massa:
-            print(key, value, end=' ')
-            print()
-            print("Не Могу взять: ")
-    for key, value in backpack.items():
-        if value > human1_massa or value > human2_massa:
-            print(key, value, end=' ')
-def contact_list():
-    note_book = {"Маша":
-                     {'tel': '+7922123561', 'vk': 'vk.com/masha321', 'youtube': 'youtube.com/masha321', 'telegram'
-                     : 't.me/masha321'},
-                 "Даша":{'tel': '+79023321293', 'vk': 'vk.com/dashulya1910', 'telegram': '@Egoistik_manyak'},
-                 "Олег":{'tel': '+79522301092', 'vk': 'vk.com/CODStyle', 'youtube': 'youtube.com/CodeSky',
-                         'telegram':'@CODProf'}}
-    user_search = input(f"Введите имя из списка контактов:Маша,Даша,Олег ").capitalize()
-    for key, value in note_book[user_search].values():
-        print(value)
+# import re
+# def isCyrillic(text):
+#     return bool(re.search('[а-яА-Я]', text))
+# def scrabl():
+#     point_en = {1:'AEIOULNSTR',
+#                     2:'DG',
+#                     3:'BCMP',
+#                     4:'FHVWY',
+#                     5:'K',
+#                     8:'JX',
+#                     10:'QZ'}
+#     point_ru = {1:'АВЕИНОСТ',
+#                 2:'ДКЛМПУ',
+#                 3:'БГЁЬЯ',
+#                 4:'ЙЫ',
+#                 5:'ЖЗЧЦЧ',
+#                 8:'ШЭЮ',
+#                 10:'ФЩЪ'}
+#     while True:
+#         text_user = input("Для отмены напишите 0. Игрок 1 Введите слово:").upper()
+#         if text_user == "0":
+#             print("Прощайте")
+#             break
+#         text_user2 = input("Для отмены напишите 0. Игрок 2 Введите слово:").upper()
+#         if text_user2 == "0":
+#             print("Прощайте")
+#             break
+#         player1 = 0
+#         player2 = 0
+#         if isCyrillic(text_user):
+#                 print('Кол-во очков:',sum([key for i in text_user for key, value in point_ru.items() if i in value]))
+#                 player1 = player1 + sum([key for i in text_user for key, value in point_ru.items() if i in value])
+#                 player2 = player2 + sum([key for i in text_user2 for key, value in point_ru.items() if i in value])
+#         else:
+#                 print('Кол-во очков:',sum([key for i in text_user for key, value in point_en.items() if i in value]))
+#                 player1 = player1 + sum([key for i in text_user for key, value in point_en.items() if i in value])
+#                 player2 = player2 + sum([key for i in text_user2 for key, value in point_en.items() if i in value])
+#         print(f"кол-во очков первого игрока {player1}, кол-во очков второго игрока {player2} ")
+# def camping():
+#     backpack = {'Зажигалка': 20, 'Компас': 100, 'Фрукты': 500, 'Рубашка': 300,
+#                 'Термос': 1000, 'аптечка': 200, 'Куртка': 600, 'Бинокль': 400,
+#                 'Удочка': 1300, 'Салфетки': 40, 'Бутерброды': 800, 'Палатка': 5500,
+#                 'Спальный мешок': 2500, 'Изолента': 250, 'Котел': 3000
+#                 }
+#     human1_massa = int(input("Введите допустимый вес рюкзака первого человека: ")) * 1000
+#     human2_massa = int(input("Введите допустимый вес рюкзака первого человека: ")) * 1000
+#     print("Могу взять: ")
+#     for key, value in backpack.items():
+#         if value < human1_massa or value < human2_massa:
+#             print(key, value, end=' ')
+#             print()
+#             print("Не Могу взять: ")
+#     for key, value in backpack.items():
+#         if value > human1_massa or value > human2_massa:
+#             print(key, value, end=' ')
+# def contact_list():
+#     note_book = {"Маша":
+#                      {'tel': '+7922123561', 'vk': 'vk.com/masha321', 'youtube': 'youtube.com/masha321', 'telegram'
+#                      : 't.me/masha321'},
+#                  "Даша":{'tel': '+79023321293', 'vk': 'vk.com/dashulya1910', 'telegram': '@Egoistik_manyak'},
+#                  "Олег":{'tel': '+79522301092', 'vk': 'vk.com/CODStyle', 'youtube': 'youtube.com/CodeSky',
+#                          'telegram':'@CODProf'}}
+#     user_search = input(f"Введите имя из списка контактов:Маша,Даша,Олег ").capitalize()
+#     for key, value in note_book[user_search].values():
+#         print(value)
 # def basnya():
 #     with open("article.txt", 'w+') as question:
 #         question.write("Вечерело \n"
@@ -711,3 +694,269 @@ def contact_list():
 #                         words.write(j)
 #     with open("articleWords", 'r') as read:
 #                 read.readlines()
+
+'''
+методы для работы с файлом
+file.close()
+file.fileno()
+file.flush() - метод очистки буфера
+file.isatty() - проверка на открытие файла в терминале()прямо сейчас)
+file.next() - возвращает след. строку файла
+file.read()
+file.readline() - чтение строки файла
+file.readlines() - чтение всех строк файла
+file.seek()
+file.seekable() - проверка доступа файла
+file.tell() - текущая позиция в строке файла
+file.truncate() - уменьшает размер файла на n байт
+file.write()
+file.writelines(line) - обавление строк в файл
+'''
+'''
+    r = только для чтения
+    w = только для записи
+    a  добавление данных в файл
+    rb = чтение бинарное
+    r+ = чтение и запись
+    rb+ = чтение и запись бинарное
+    w+ = чтение и запись, если файла нет то создаём его
+    wb+ = аналогично выше стоящему
+    a+ = чтение и добавление, если файла нет то создаём его
+    ab = аналогично выше стоящему только бинарное
+    ab+ = аналог выше стоящего с созданием файла
+'''
+#задание 1
+def basnya():
+    with open("article.txt", 'w+') as question:
+        question.write("Вечерело \n"
+                   "Жужжали мухи \n"
+                   "Кипела вода в чайнике \n"
+                   "Венера зажглась на небе \n"
+                   "Деревья шумели \n"
+                   "Тучи разошлись \n"
+                   "Листва Зеленела \n")
+    with open ("article.txt", 'r') as file:
+        for i in file.readlines():
+            print(i, end='')
+
+
+def task2():
+    """Задание 2: Создать файл со словами из 7+ букв из басни"""
+    try:
+        with open('vorona_i_lisitsa.txt', 'r') as file:
+            content = file.read()
+
+        words = content.split()
+        long_words = [word for word in words if len(word.strip('.,!?;:"')) >= 7]
+
+        with open('long_words.txt', 'w') as output_file:
+            for word in long_words:
+                output_file.write(word + '\n')
+
+        print(f"Задание 2 выполнено! Найдено {len(long_words)} слов из 7+ букв.")
+        print("Результат записан в файл 'long_words.txt'")
+
+    except FileNotFoundError:
+        print("Ошибка: Файл 'vorona_i_lisitsa.txt' не найден!")
+        print("Создайте файл с басней или измените имя файла.")
+
+def task3():
+    try:
+        with open('file1.txt', 'r') as file1:
+            lines = file1.readlines()
+
+        with open('file2.txt', 'w') as file2:
+            for line in lines:
+                file2.write(line)
+
+        print("файлы переписаны, хорошего дня!")
+
+    except FileNotFoundError:
+        print("Ошибка: Файл 'file1.txt' не найден!")
+
+
+def task4():
+    try:
+        with open('file1.txt', 'r', encoding='utf-8') as file1:
+            lines = file1.readlines()
+
+        with open('file2_reversed.txt', 'w', encoding='utf-8') as file2:
+            for line in reversed(lines):
+                file2.write(line)
+
+        print("Задание 4 выполнено! Строки переписаны в обратном порядке в 'file2_reversed.txt'")
+
+    except FileNotFoundError:
+        print("Ошибка: Файл 'file1.txt' не найден!")
+
+
+def task5():
+    try:
+        with open('text_file.txt', 'r') as file:
+            lines = file.readlines()
+
+        last_no_comma_index = -1
+        for i, line in enumerate(lines):
+            if ',' not in line:
+                last_no_comma_index = i
+
+        if last_no_comma_index != -1:
+            lines.insert(last_no_comma_index + 1, '*' * 12 + '\n')
+        else:
+            lines.append('*' * 12 + '\n')
+
+        with open('text_file.txt', 'w') as file:
+            file.writelines(lines)
+
+        print("Задача выполнена!")
+
+    except FileNotFoundError:
+        print("Ошибка: Файл 'text_file.txt' не найден!")
+
+
+def task6():
+    try:
+        char = input("Введите символ для поиска: ").strip()
+        if len(char) != 1:
+            print("Ошибка: нужно ввести один символ!")
+            return
+
+        with open('text_file.txt', 'r') as file:
+            content = file.read()
+
+        words = content.split()
+        count = 0
+        matching_words = []
+
+        for word in words:
+            clean_word = word.strip('.,!?;:"').lower()
+            if clean_word and clean_word[0] == char.lower():
+                count += 1
+                matching_words.append(word)
+
+        print(f"Найдено {count} слов, начинающихся с символа '{char}':")
+        for word in matching_words[:10]:  # Показываем первые 10 слов
+            print(f"  {word}")
+        if len(matching_words) > 10:
+            print(f"  ... и еще {len(matching_words) - 10} слов")
+
+    except FileNotFoundError:
+        print("Ошибка: Файл 'text_file.txt' не найден!")
+
+
+def task7():
+    try:
+        with open('file_with_stars.txt', 'r', encoding='utf-8') as file1:
+            lines = file1.readlines()
+
+        modified_lines = [line.replace('*', '&') for line in lines]
+
+        with open('file_with_ampersands.txt', 'w', encoding='utf-8') as file2:
+            file2.writelines(modified_lines)
+
+        print("Задача выполнена! Символы * заменены на & в новом файле.")
+
+    except FileNotFoundError:
+        print("Ошибка: Файл 'file_with_stars.txt' не найден!")
+
+
+def task8():
+    strings = [
+        "Первая строка",
+        "Вторая строка",
+        "Третья строка",
+        "Четвертая строка",
+        "Пятая строка"
+    ]
+
+    print("Массив для записи:")
+    for i, string in enumerate(strings, 1):
+        print(f"{i}. {string}")
+
+    with open('array_output.txt', 'w') as file:
+        for string in strings:
+            file.write(string + '\n')
+
+    print("Задача выполнена! Массив записан в файл 'array_output.txt'")
+
+
+def task9():
+    try:
+        filename = input("Введите имя файла (по умолчанию 'text_file.txt'): ").strip()
+        if not filename:
+            filename = 'text_file.txt'
+
+        with open(filename, 'r') as file:
+            content = file.read()
+
+        char_count = len(content)
+        print(f"Файл '{filename}' содержит {char_count} символов.")
+
+    except FileNotFoundError:
+        print(f"Ошибка: Файл '{filename}' не найден!")
+
+
+def task10():
+    try:
+        filename = input("Введите имя файла (по умолчанию 'text_file.txt'): ").strip()
+        if not filename:
+            filename = 'text_file.txt'
+
+        with open(filename, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+
+        line_count = len(lines)
+        print(f"Файл '{filename}' содержит {line_count} строк.")
+
+    except FileNotFoundError:
+        print(f"Ошибка: Файл '{filename}' не найден!")
+
+def main():
+    while True:
+        print("\n" + "=" * 50)
+        print("Меню: \n"
+                    "1. Создать примеры файлов для демонстрации \n"
+                    "2. Задание 2 - Слова из 7+ букв из басни \n"
+                    "3. Задание 3 - Переписать строки с сохранением порядка \n"
+                    "4. Задание 4 - Переписать строки в обратном порядке \n"
+                    "5. Задание 5 - Добавить строку из звездочек \n"
+                    "6. Задание 6 - Подсчитать слова по первой букве \n"
+                    "7. Задание 7 - Заменить * на & \n"
+                    "8. Задание 8 - Записать массив в файл \n"
+                    "9. Задание 9 - Подсчитать символы в файле \n"
+                    "10. Задание 10 - Подсчитать строки в файле \n"
+                    "0. Выход")
+
+        choice = input("Выберите задание (0-10): ").strip()
+
+        if choice == '0':
+            print("Выход из программы.")
+            break
+        elif choice == '1':
+            basnya()
+        elif choice == '2':
+            task2()
+        elif choice == '3':
+            task3()
+        elif choice == '4':
+            task4()
+        elif choice == '5':
+            task5()
+        elif choice == '6':
+            task6()
+        elif choice == '7':
+            task7()
+        elif choice == '8':
+            task8()
+        elif choice == '9':
+            task9()
+        elif choice == '10':
+            task10()
+        else:
+            print("Неверный выбор! Попробуйте снова.")
+
+        input("\nНажмите Enter для продолжения...")
+
+
+if __name__ == "__main__":
+    main()
